7ea3c4902d24e25c245115ecace3db8efea042bd
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2020-11-01 23:50:43 +0700

    -

--- week12send1/Source.cpp
+++ week12send1/Source.cpp
@@ -4,7 +4,7 @@ int main()
 	int num[5], x, y, temp;
 	printf("Sort 5 numbers\n");
 	printf("Enter 5 numbers,seperate by space\n");
-	printf("Enter number : ");
+	printf("Enter number  ");
 	scanf_s("%d %d %d %d %d", &num[0], &num[1], &num[2], &num[3], &num[4]);
 
 	for (x = 0; x < 5; x++)
@@ -19,7 +19,7 @@ int main()
 			}
 		}
 	}
-	printf("Number is :");
+	printf("Number is ");
 	for (x = 0; x < 5; x++)
 	{
 		printf("%d\t", num[x]);
